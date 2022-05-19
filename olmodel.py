d36698f4eca41f1ef27930a72db9281714f7c137
shepardxia <78109174+shepardxia@users.noreply.github.com>
2022-05-19 18:32:36 -0500

    Update olmodel.py

--- olmodel.py
+++ olmodel.py
@@ -4,6 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import json as js
 import PIL.Image as Image
+import time
 
 
 def load_pb(path_to_pb):
@@ -18,16 +19,26 @@ model_path = './weights/weights_plate_v1.0.0.pb'
 graph = load_pb(model_path)
 sess = tf.compat.v1.Session(graph=graph)
 
-image = cv2.imread('./images/10.jpg')
-#cv2.imshow('image', image)
-np_images = np.array([image])
 
 
 image_tensor = graph.get_tensor_by_name('image_tensor: 0')
 num_det = graph.get_tensor_by_name('num_detections: 0')
 det_scores = graph.get_tensor_by_name('detection_scores: 0')
 det_boxes = graph.get_tensor_by_name('detection_boxes: 0')
-num_boxes, scores, boxes = sess.run([num_det, det_scores, det_boxes], feed_dict={image_tensor: np_images})
+
+before = time.time()
+print(before)
+
+for i in range(100):
+    image = cv2.imread('./images/10.jpg')
+    np_images = np.array([image])
+    num_boxes, scores, boxes = sess.run([num_det, det_scores, det_boxes], feed_dict={image_tensor: np_images})
+
+after = time.time()
+print(after)
+
+print("total time used:", (after - before))
+
 rets = []
 
 for i in range(len(scores[0])):
@@ -35,10 +46,10 @@ for i in range(len(scores[0])):
         rets.append(boxes[0][i].tolist())
 
 
-ret_img = tf.image.draw_bounding_boxes(np_images, [rets], [[255, 0, 0]])
-print(ret_img)
-plt.imshow(tf.cast(ret_img[0], tf.int32))
-plt.show()
+#ret_img = tf.image.draw_bounding_boxes(np_images, [rets], [[255, 0, 0]])
+#print(ret_img)
+#plt.imshow(tf.cast(ret_img[0], tf.int32))
+#plt.show()
 
 #cv2.imshow('hey', ret_img[0].numpy())
 #cv2.waitKey(0)
